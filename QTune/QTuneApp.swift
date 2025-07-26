d22d606da219ffe196d8c6c9f1d3d92001df45b5
inju2403 <56947879+inju2403@users.noreply.github.com>
2025-07-26 16:06:38 +0900

    init project

---
+++ QTune/QTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  QTuneApp.swift
+//  QTune
+//
+//  Created by 이승주 on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct QTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
