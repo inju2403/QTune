811a4f3bcece2a38f40b360f5eeb5b8d69c6c6be
inju2403 <56947879+inju2403@users.noreply.github.com>
2026-01-03 20:32:31 +0900

    chore: bump build to 105

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -12,7 +12,7 @@ let project = Project(
       infoPlist: .extendingDefault(with: [
         "CFBundleDisplayName": "QTune",
         "CFBundleShortVersionString": "1.0.0",
-        "CFBundleVersion": "104",
+        "CFBundleVersion": "105",
         "UILaunchStoryboardName": "LaunchScreen",
         "UIViewControllerBasedStatusBarAppearance": true
       ]),
