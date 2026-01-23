6a39ec5ed9755d06bb605e7d6dc028df4cb8ea3e
inju2403 <56947879+inju2403@users.noreply.github.com>
2026-01-23 11:13:12 +0900

    [VER] 1.4.1(146)

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -14,8 +14,8 @@ let project = Project(
       deploymentTargets: .iOS("17.0"),
       infoPlist: .extendingDefault(with: [
         "CFBundleDisplayName": "QTune",
-        "CFBundleShortVersionString": "1.4.0",
-        "CFBundleVersion": "140",
+        "CFBundleShortVersionString": "1.4.1",
+        "CFBundleVersion": "146",
         "UILaunchStoryboardName": "LaunchScreen",
         "UIViewControllerBasedStatusBarAppearance": true,
         "UIUserInterfaceStyle": "Light"
